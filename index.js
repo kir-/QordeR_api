385c7db0aee8bef2c716508135a398eba9287369
changbhc@gmail.com <changbhc@gmail.com>
2019-09-07 23:34:42 -0700

    commit before ggpull

--- index.js
+++ index.js
@@ -139,6 +139,106 @@ app.post('/api/upgradeStatus/:orderId', (req, res) => {
     });
 });
 
+app.post('/login', (req, res) => {
+  const user = req.body.email;
+  const password = req.body.password;
+  const queryConfig = {
+    text: `SELECT id, password FROM restaurants WHERE username = $1 AND password = $2`,
+    values: [user, password]
+  };
+  db.query(queryConfig)
+    .then((response) => {
+      const restaurantId = response.rows[0].id;
+      req.universalCookies.set('user', restaurantId);
+      res.send(`/admin/${restaurantId}`);
+    })
+    .catch((error) => {
+      res.send(`/admin`);
+    });
+});
+
+app.post('/logout', (req, res) => {
+  res.send(`/admin`);
+});
+
+app.get('/restaurant/:id', (req, res) => {
+  const queryConfig = {
+    text: "SELECT * FROM tables WHERE restaurant_id = $1",
+    values: [req.params.id]
+  };
+});
+
+app.get('/:table_id', (req, res) => { //creates new order is table is empty or adds to current order
+  const queryConfig = {
+    text: "SELECT current_number_customers FROM tables WHERE id = $1",
+    values: [req.params.table_id]
+  };
+  db.query(queryConfig)
+    .then((response) => {
+      // const restaurantId = response.rows[0].id;
+      const customers = response.rows[0].current_number_customers;
+      console.log(customers);
+      if (customers === 0) {
+        const queryConfig = {
+          text: "INSERT into orders (table_id, completed) VALUES ($1, FALSE) RETURNING id",
+          values: [req.params.table_id]
+        };
+        db.query(queryConfig)
+          .then(
+            (response) => {
+              const queryConfig = {
+                text: `UPDATE tables SET current_number_customers = 1 WHERE id = $1`,
+                values: [req.params.table_id]
+              };
+              db.query(queryConfig);
+              res.send(response.rows[0]);
+            });
+      } else {
+        const queryConfig = {
+          text: "SELECT id FROM orders WHERE table_id = $1 AND completed = FALSE",
+          values: [req.params.table_id]
+        };
+        db.query(queryConfig)
+          .then((response) => {
+            const queryConfig = {
+              text: `UPDATE tables SET current_number_customers = ${customers + 1} WHERE id = $1`,
+              values: [req.params.table_id]
+            };
+            db.query(queryConfig);
+            res.send(response.rows[0]);
+          });
+      }
+      // req.session.user = restaurantId;
+      // res.send(`/restaurant/${restaurantId}`);
+    });
+});
+
+app.post('/:table_id/order', (req, res) => { // accepts array called orders [{item_id, quantity}] and adds to database
+  const queryConfig = {
+    text: "SELECT id FROM orders WHERE table_id = $1 AND completed = FALSE",
+    values: [req.params.table_id]
+  };
+  console.log(`table id: ${req.params.table_id}`);
+  db.query(queryConfig)
+    .then((response) => {
+      console.log(`order id: ${response.rows[0].id}`);
+      console.log(`body: ${req.body.order}`);
+      for (let item of req.body.order) {
+        const queryConfig = {
+          text: "INSERT into order_details (item_id, order_id, quantity) VALUES ($1, $2, $3)",
+          values: [item.id, response.rows[0].id, item.quantity]
+        };
+        db.query(queryConfig)
+          .then(() => {
+            console.log(`item id: ${item.id}, item quantity: ${item.quantity}`);
+            if (req.body.order[req.body.order.length - 1].id === item.id) {
+              res.send("success");
+            }
+          });
+      }
+    });
+});
+
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname+'/client/build/index.html'));
